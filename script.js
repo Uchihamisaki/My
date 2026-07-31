9e963a4b191ee1a416c069b1c897b48ba847fdba
Uchihamisaki <artistaisha14@outlook.com>
2026-07-31 19:19:44 +0300

    Replace first artwork with new detailed description
    Updated artwork details in local storage with a new entry.

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const artworks = JSON.parse(localStorage.getItem('myArtworks')) || [
         {
-            title: "First Light",
-            description: "This is a sample artistic description for the first artwork. You can delete or edit this from your dashboard.",
-            image: "https://images.unsplash.com/photo-1579783902614-a3fb3927b675?auto=format&fit=crop&w=800&q=80"
+            title: "The beginning of the end",
+description: " Dreams do not succumb to the laws of our reality\nThis artwork captures the moment of the Beginning of the End, inspired by my dreams and the emotions of my reality. At the cliff's top sits an ancient tree trunk engraved with a riddle visible only in its mirror reflection, linking my identity and age to the piece. On that same trunk, a single last strand of hair clings to the world, proving the girl’s existence. Nearby lies a broken lamp with stars pouring out, a reminder that true light emerges from every fracture.\nI used acrylics, colored pencils, and watercolors to create contrast between the ocean water, the blood moon, and the drowning planets. ​That radiant star is a memory of my late friend. The three stars link my nationality to my painting. Through this exploration, I learned that the ocean protects, supports, and inspires us. It grants us the courage to sink into the unknown without fear, transforming our earthly end into a vast beginning. Pollution erodes our safety, masking the ocean's beauty, and perhaps preventing it from truly being an ocean. I painted myself falling in the center not in fear, but in a fall of surrender and peace. Below stretches the ocean of infinity, where planets and black holes sink beneath a Blood Moon; I placed the Blood Moon there, believing that it is a symbol of a beginning. This proves that the ocean is our space on Earth. I believe that despite human impact, destiny will lead the ocean to shine again. My message is to reflect on the end we all fear: for what we perceive as an end is, in truth, the beginning of a wider freedom.\nSo ......I did not deny the cruelty of reality, yet I never surrendered to it.",
+image: "https://i.ibb.co/XGSkcmf/02.jpg"
+
         }
     ];
 
